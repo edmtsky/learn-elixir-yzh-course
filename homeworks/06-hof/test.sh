8fc81c561ec19a7fd76109df27a7ff0a3624601b
edmtsky <skythoren@gmail.com>
2025-04-08 14:52:06 +0300

    add 06-homework

---
+++ homeworks/06-hof/test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# elixirc world_champ.ex && elixir world_champ_test.exs
+# elixirc code_stat.ex && elixir code_stat_test.exs
+
+elixir world_champ.ex world_champ_test.exs
+elixir code_stat.ex code_stat_test.exs
