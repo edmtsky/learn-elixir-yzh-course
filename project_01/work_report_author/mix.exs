ba525d7b26f1d40e9fcd92eb6b9a060f0e8147e1
edmtsky <skythoren@gmail.com>
2025-04-14 12:41:15 +0300

    update elixir up to 1.18, update Makefile

--- project_01/work_report_author/mix.exs
+++ project_01/work_report_author/mix.exs
@@ -5,7 +5,7 @@ defmodule WorkReport.MixProject do
     [
       app: :work_report,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       escript: [main_module: WorkReport, app: nil]
